5eb3aa8f94f61088bb29c65f9bf3223340b55542
Ivan <darkside8888@mail.ru>
2019-03-31 23:20:28 +0400

    JS-4 task completed [Соловьев]

--- index.js
+++ index.js
@@ -3,5 +3,13 @@
  * @returns {String[]}
  */
 module.exports = function (tweet) {
+	var arr = tweet.split(" ");
+	var resArr = [];
 
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i][0] == '#') {
+			resArr.push(arr[i].slice(1));
+		}		
+	}
+	return resArr;
 };
