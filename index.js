2518e8d70820cfc32a1eb3351b42052851f83436
NadyaNikishina <45035053+NadyaNikishina@users.noreply.github.com>
2019-04-01 14:06:05 +0400

    js-4 done Nikishina

--- index.js
+++ index.js
@@ -3,5 +3,16 @@
  * @returns {String[]}
  */
 module.exports = function (tweet) {
-
+       
+    var tags = tweet.match(/#[\wа-яё0-9]+/gi);
+    var result = [];
+    if (tags === null) {
+        return result;
+    }
+    else {
+    for(var i=0; i < tags.length; i++) {
+        result.push(tags[i].slice(1));
+    }
+    }
+    return result;
 };
