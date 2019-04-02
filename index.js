ca188a20d77a933812250825e66cc6b412840d70
max-barabash <48650035+max-barabash@users.noreply.github.com>
2019-04-02 15:16:55 +0400

    Add function

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 /**
+ * Барабаш Максим Сергеевич
  * @param {String} tweet
  * @returns {String[]}
  */
 module.exports = function (tweet) {
-
+    var source = tweet.split(' ');
+    for (var i = source.length-1; i >= 0; i--) {
+        if ((source[i]).indexOf('#') === -1) {
+            source.splice(i, 1);
+        }
+        else {
+            source[i] = source[i].replace('#', '')
+        }
+    }
+    return source;
 };
