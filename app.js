da93327478aac22e0838d54380cc69ab86f9ee65
Vlad Zhichkin <v.zhichkin@yandex.ru>
2019-04-05 23:27:55 +0300

    Zhichkin. Первая реализация задания готова.

--- app.js
+++ app.js
@@ -1,3 +1,2 @@
-var addTime = require('./index.js');
-console.info(addTime(12, 30, 30)); // 13:00
-console.info(addTime(23, 59, 31)); // 00:30
+var getHashTags = require('./index.js'); 
+console.info(getHashTags('Прохожу курс на #pravo по #jsvascript')); // ["pravo", "jsvascript"]
--- index.js
+++ index.js
@@ -3,5 +3,34 @@
  * @returns {String[]}
  */
 module.exports = function (tweet) {
+    let tempTweet = tweet;
+    let arrayHashTag = [];
+    
+    if (tempTweet.indexOf('#') !== -1){
+        let stringArray = tempTweet.split(" ");
+        for (var i = 0; i < stringArray.length; i++) {
+            var tweet = stringArray[i];
+            if (tweet.indexOf('#') !== -1) {
+                arrayHashTag.push(tweet.substring(1));
+            }
+        }
+        return arrayHashTag;
+    } 
+    else {
+        return arrayHashTag;
+    }
 
 };
+
+
+
+
+
+//for(var str in stringArray)
+//	console.log(stringArray[str]);
+
+
+//Строка содержит только буквы русского и латинского алфавита, знак решетки и пробелы. 
+//Гарантируется, что в функцию передается непустая строка. Слова в строке разделены одним пробелом. 
+//Хештег начинается со знака решетки (#) и состоит из одного слова.
+//В результирующем массиве хештеги должны быть без решетки. Если в слове хештегов нет, то возвращается пустой массив.
